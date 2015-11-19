ef93ef7c854475926fe42521fcd082d4a2243deb
Karolina Munoz <karolinamv05@gmail.com>
2015-11-19 00:23:31 -0600

    Video panel finished

--- app.js
+++
@@ -1,4 +0,0 @@
-
-
-angular.module('polymer', ['polymer.controllers' ])
-
--- controllers.js
+++
@@ -1,8 +0,0 @@
-angular.module('polymer.controllers',[])
-
-.controller('HomeController',['$scope',function($scope){
-  $scope.imgUrl="";
-  $scope.send= function(url){
-      $scope.imgUrl=url;
-  }
-}])
